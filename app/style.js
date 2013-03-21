9ab69e87cd92e5c495af34e1798be275498e37e1
Kendell <contact@kendelljoseph.com>
2013-03-21 16:25:29 -0500

    Updating SWPM

--- app/style.js
+++ app/style.js
@@ -1,6 +1,17 @@
 // Style
 $(function(){
 
-console.log('js style');
-
+	$('.notselected').hover(function(){
+		$(this).css({backgroundColor: '#6699CC'});
+	},function(){
+		$(this).css({backgroundColor: '#fff'});
+	});
+	
+	$('.tabs ').click(function(){
+		var isSelected = $(this).attr('class') == 'selected';
+		if(isSelected) return;
+		$('.selected').removeClass('selected');
+		$(this).addClass('selected');
+		$(this).removeClass('notselected');
+	});
 });
